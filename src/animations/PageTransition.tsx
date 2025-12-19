6575c0eba73c64006273601def1264f7bba53542
truelibrary <trueislamlibrary1@gmail.com>
2025-12-19 19:05:58 +0000

    fix: add padding for mobile

--- src/animations/PageTransition.tsx
+++ src/animations/PageTransition.tsx
@@ -1,11 +1,12 @@
 import { motion } from "framer-motion";
+import classes from "./PageTransition.module.css";
 
 const PageTransition = (Component: React.ComponentType) => (
   <motion.div
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
     transition={{ duration: 0.6, ease: "easeOut" }}
-    className="p-4"
+    className={classes.container}
   >
     <Component />
   </motion.div>
