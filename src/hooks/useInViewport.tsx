ed6cfaf4d03df60b070d24aa4378af84c4ce815f
enjoyerofcoffee <47926269+enjoyerofcoffee@users.noreply.github.com>
2026-04-23 22:47:32 +0100

    revert back useInViewport

--- src/hooks/useInViewport.tsx
+++ src/hooks/useInViewport.tsx
@@ -1,20 +1,38 @@
-import { useState, useEffect, type RefObject } from "react";
+import { useState, useEffect, useCallback, type RefObject } from "react";
+
+function isElementInViewport(el: Element) {
+  const rect = el.getBoundingClientRect();
+
+  return (
+    rect.bottom >= 0 &&
+    rect.right >= 0 &&
+    rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
 export const useInViewport = (ref: RefObject<Element | null>) => {
   const [isVisible, setIsVisible] = useState(true);
 
-  useEffect(() => {
-    const el = ref.current;
-    if (!el) return;
-
-    const observer = new IntersectionObserver(
-      ([entry]) => setIsVisible(entry.isIntersecting),
-      { threshold: 0 }
-    );
-
-    observer.observe(el);
-    return () => observer.disconnect();
+  const update = useCallback(() => {
+    if (ref.current) {
+      setIsVisible(isElementInViewport(ref.current));
+    }
   }, [ref]);
 
-  return { isVisible };
+  useEffect(() => {
+    ["scroll", "load", "DOMContentLoaded", "resize", "click"].forEach(
+      (type) => {
+        window.addEventListener(type, update);
+      },
+    );
+    return () => {
+      ["scroll", "load", "DOMContentLoaded", "resize", "click"].forEach(
+        (type) => {
+          window.removeEventListener(type, update);
+        },
+      );
+    };
+  }, [update]);
+  return { isVisible, update };
 };
